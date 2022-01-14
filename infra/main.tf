cf1444d97c25d62a37f565a0486de445f27e843d
Aishu1503 <97735852+Aishu1503@users.noreply.github.com>
2022-01-14 17:56:53 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,30 +1,16 @@
 provider "aws" {
-    region = "us-east-2"
+region = "us-east-1"
 }
 
-
-variable "image" {
-  default = "ami-0603cbe34fd08cb81"
+variable "hw" {
+default = "t2.medium"
 }
 
-variable "hardware" {
-  default = "t2.medium"
+resource "aws_instance" "b1" {
+ami = "ami-083602cee93914c0c"
+instance_type = var.hw
+tags = {
+Name = "server1"
+Env = "Dev"
 }
-
-variable "name" {
- default = "server1"
-}
-
-resource "aws_instance" "web" {
-  ami           = var.image
-  instance_type = var.hardware
-
-  tags = {
-    Name = var.name
-    Env = upper("production")
-  }
-}
-
-output "public_ip" {
-  value = aws_instance.web.public_ip
 }
